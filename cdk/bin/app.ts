03191236b84b8639d3d5e71bc9340411b9d0456e
xchu <xchu@pop-os.localdomain>
2024-05-13 21:23:53 -0500

    fixed context

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -3,7 +3,13 @@ import { EcsStack } from '../lib/ecs-stack';
 
 const app = new cdk.App();
 
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
 new EcsStack(app, 'EcsStack', {
+  env: env,
   imageTag: app.node.tryGetContext('imageTag'),
   ecrRepo: app.node.tryGetContext('ecrRepo'),
   containerPort: parseInt(app.node.tryGetContext('containerPort')),
