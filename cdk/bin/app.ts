cf4bd00b0a92d9416ce680e4783198099e53f095
xchu <xchu@pop-os.localdomain>
2024-05-13 20:40:44 -0500

    added deploy code

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -3,11 +3,8 @@ import { EcsStack } from '../lib/ecs-stack';
 
 const app = new cdk.App();
 new EcsStack(app, 'EcsStack', {
-  env: {
-    region: process.env.CDK_DEFAULT_REGION, // Ensure these are set in Jenkins or the environment
-    account: process.env.CDK_DEFAULT_ACCOUNT
-  },
   imageTag: app.node.tryGetContext('imageTag'),
   ecrRepo: app.node.tryGetContext('ecrRepo'),
   containerPort: parseInt(app.node.tryGetContext('containerPort')),
+  stackName: app.node.tryGetContext('stackName')
 });
