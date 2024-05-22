8fa580b204685dba588694a2be1707aabcc72579
xcandyhu <46210506+xcandyhu@users.noreply.github.com>
2024-05-22 14:17:40 -0400

    Add comments and clean up (#4)
    * no need to configure port for ecs
    
    * fix alb sg
    
    * add report url step
    
    * remove export
    
    ---------
    
    Co-authored-by: xchu <xchu@pop-os.localdomain>

--- cdk/lib/ecs-stack.ts
+++ cdk/lib/ecs-stack.ts
@@ -7,7 +7,6 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 interface EcsStackProps extends cdk.StackProps {
   readonly imageTag: string;
   readonly ecrRepo: string;
-  readonly containerPort: number;
 }
 
 class EcsStack extends cdk.Stack {
@@ -16,10 +15,9 @@ class EcsStack extends cdk.Stack {
 
     const vpc = ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true });
 
-    // Create an ECS cluster
+    // Create an ECS cluster and add EC2 capacity
     const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
 
-    // Add EC2 capacity to the cluster
     cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
       instanceType: new ec2.InstanceType('t2.micro'),
       desiredCapacity: 1,
@@ -27,6 +25,7 @@ class EcsStack extends cdk.Stack {
       maxCapacity: 1
     });
 
+    // Create a task definition and add a container
     const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef', {
       networkMode: ecs.NetworkMode.AWS_VPC,
     });
@@ -37,10 +36,11 @@ class EcsStack extends cdk.Stack {
     });
 
     container.addPortMappings({
-      containerPort: props.containerPort,
+      containerPort: 8080,
       protocol: ecs.Protocol.TCP,
     });
 
+    // create two SGs, one for the alb and one for the ECS service
     const albSecurityGroup = new ec2.SecurityGroup(this, 'AlbSecurityGroup', {
       vpc,
       allowAllOutbound: true,
@@ -53,9 +53,14 @@ class EcsStack extends cdk.Stack {
       allowAllOutbound: true,
       description: 'Allow inbound traffic from ALB',
     });
-    serviceSecurityGroup.addIngressRule(albSecurityGroup, ec2.Port.tcp(props.containerPort));
+    serviceSecurityGroup.addIngressRule(albSecurityGroup, ec2.Port.tcp(8080));
 
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'alb', { vpc, internetFacing: true });
+    // create the alb and ECS service and add the latter as a target group
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'alb', { 
+      vpc, 
+      internetFacing: true,
+      securityGroup: albSecurityGroup
+    });
     const listener = alb.addListener('listener', { port: 80 });
 
     const ecsService = new ecs.Ec2Service(this, 'ClinicService', {
